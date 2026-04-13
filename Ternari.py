938ed950db60287506eb2b96e85efbf5cf166a7f
Ritom kumar Hajra <ritomk61@gmail.com>
2026-04-13 23:24:58 +0530

    update

--- Ternari.py
+++ Ternari.py
@@ -6,18 +6,20 @@ class student:
         self.adress = adress
     
 class teacher(student):
-    def __init__(self, name, sysid, adress,tname,spe,rate):
+    def __init__(self, name, sysid, adress,tname,spe,rate,gen):
         super().__init__(name, sysid, adress)
         self.tname = tname
         self.spe = spe
         self.rate = rate
-        # self.gen = gen
+        self.gen = gen
     def show(self):
         print(f"Hello!Your name is {self.name}.Your College Name is {self.su}.Your college system id {self.sysid}.Your Adress is {self.adress}")
     def showt(self):
-        print(f"Your faculty name is {self.tname}.She is specielest in {self.spe} and He/She have rated in their subject in {self.rate}")
+        status = "Excellent teacher" if self.rate == 5 else "Good Teacher"
+        gender = "He" if self.gen == "male" else  "She" #* Ternari operator
+        print(f"Your faculty name is {self.tname}.{gender} is specielest in {self.spe} and {gender} have rated in their subject in {self.rate} {status}")
 
-d = teacher("Ritom" , 2025834055 , "Bangladesh" , "Smita Tiwary" , "Python" , 5)
+d = teacher("Ritom" , 2025834055 , "Bangladesh" , "Smita Tiwary" , "Python" , 4 , "male")
 d.show()
 d.showt()
         
